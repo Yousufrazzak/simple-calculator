6d23f1b579b7a1f95dc710ec32fc2f60a6a2bb96
Yousuf Razzak <usuf.razzak12344321@gmail.com>
2024-03-25 21:06:12 +0500

    final calculator

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {message: "EnterFirstNumber", type:"number", name:"Fnumber"},
+    {message:"EnterSecondNumber", type:"number", name:"Snumber" },
+    {message:"Select Operation", type: "list", name:"action", choices:
+    ["Addition","Subtraction","Multiplication","Division"],
+
+    }
+]);
+
+if (answer.action==="Addition"){
+console.log(answer.Fnumber + answer.Snumber);
+}
+else if (answer.action==="Subtraction") {
+    console.log(answer.Fnumber - answer.Snumber);
+}
+else if (answer.action==="Multiplication"){
+    console.log(answer.Fnumber * answer.Snumber);
+}
+else if (answer.action==="Division"){
+    console.log(answer.Fnumber / answer.Snumber);
+}
+else{
+    console.log("enter your value");
+}
